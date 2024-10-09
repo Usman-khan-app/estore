04bc1e77f5d9452f1fcb6f9f1e9fc52a1106eb62
Usman-khan-app <157116414+Usman-khan-app@users.noreply.github.com>
2024-10-09 12:55:18 +0500

    c
    c

--- src/components/Home/Mainproduct.jsx
+++ src/components/Home/Mainproduct.jsx
@@ -49,7 +49,7 @@ export default function Mainproduct() {
 
                 {/* //=========================================================== */}
                 <div className="md:inline-flex  mx-4 md:mx-40 gap-8 border mb-5 rounded-md shadow-sm" role="group">
-  <button onClick={() => setFilter(products)} type="button" className="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-s-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
+  <button onClick={() => setFilter(products)} type="button" className="px-4 py-2 text-sm font-medium text-gray-900 bg-blue-900 border border-gray-900 rounded-s-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
     All Products
   </button>
   <button onClick={() => filterProducts("men's clothing")} type="button" className="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent  border border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
